e9acd40e373a427307d128a8994f24bc143e6116
Mikhail Kuzmin <mikhalkuzmin@yandex.ru>
2022-12-11 22:57:45 +0400

    Full task seminar 5 added

---
+++ main.py
@@ -0,0 +1,11 @@
+# Задача №38: Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+my_text = 'Текст абв проверкиабв програбвммы проверки программы, удаляющую из \
+    этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda x: 'абв' not in x, my_text.split()))
+    return " ".join(my_text)
+
+my_text = del_some_words(my_text)
+print(my_text)
